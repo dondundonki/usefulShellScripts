1c2fbbc4adc0b3b1fcb2b0dc72bbd861d1d76ac1
Don Seo <104734810+dondundonki@users.noreply.github.com>
2023-10-27 14:43:48 -0400

    Fetch Latest Git
    This shell script automatically checks the given root directory and identifies all Git directory and pulls the latest changes.

---
+++ fetch_latest_git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the directory containing your repositories
+repo_directory="/Users/donseo/newAI"
+
+# Loop through each directory in the specified path
+for dir in "$repo_directory"/*; do
+  if [ -d "$dir" ]; then
+    # Check if it's a Git repository
+    if [ -d "$dir/.git" ]; then
+      # Change into the repository directory
+      cd "$dir"
+      result=$(basename "$dir")
+      printf "\nChecking for updates in $result\n"
+      # Perform a git pull
+      git pull
+      # Return to the original directory
+      cd "$repo_directory"
+    else
+      printf "Skipping non-Git directory: $dir"
+    fi
+  fi
+done
+
